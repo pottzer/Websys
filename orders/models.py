2ead684c66bc65e60117671e59abf45eec990c9d
root <root@capsize.capsize>
2014-11-24 15:11:37 +0000

    Added orders app with models

---
+++ orders/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from users.models import User
+from goods.models import Goods
+from django.utils import timezone
+
+class Order(models.Model):
+	orderID = models.IntegerField(primary_key=True)
+	username = models.ForeignKey(User)
+	date = models.DateTimeField(default=timezone.now())
+
+
+class OrderItems(models.Model):
+	id_goods = models.ForeignKey(Goods)
+	orderID = models.ForeignKey(Order)
+	price = models.IntegetField()
+	name = models.CharField(max_length=255)
+
+
